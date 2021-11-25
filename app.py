e495dcab41c05dae7ee74425fc2d82357e3a3f2b
Ping <pingkorat@gmail.com>
2021-11-25 11:12:10 +0700

    update

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, jsonify
+from pyzbar import pyzbar
+from PIL import Image
+import requests
 
 app = Flask(__name__)
 
@@ -29,5 +32,41 @@ def hello():
 def get_api():
     return jsonify(data)
 
+
+@app.route('/foo', methods=['POST'])
+def foo():
+    data = request.json
+    return jsonify(data)
+
+
 if __name__ == "__main__":
     app.run(debug=False)
+
+
+"""
+def qr(request):
+    if request.method == 'POST':
+        if 'image' in request.files:
+            image = request.files['image']
+            barcodes = pyzbar.decode(Image.open(image))
+
+            results = []
+            for barcode in barcodes:
+                results.append({
+                    'code': barcode.data.decode("utf-8"),
+                    'type': barcode.type,
+                    'postion': {
+                        'x': barcode.rect.left,
+                        'y': barcode.rect.top,
+                        'w': barcode.rect.width,
+                        'h': barcode.rect.height
+                    }
+                })
+
+            if results:
+                return jsonify({'results': results})
+            else:
+                return jsonify({'error': 'NO_DETECT'})
+
+    return jsonify({'ok': True})
+"""
