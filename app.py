b6ed100b34e6b12df81197becf9afae09a9aaf3b
Ping <pingkorat@gmail.com>
2021-11-25 11:22:37 +0700

    update

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/foo', methods=['POST'])
+def foo():
+    data = request.json
+    return jsonify(data)
+
+
+"""
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -31,3 +42,5 @@ def get_api():
 
 if __name__ == "__main__":
     app.run(debug=False)
+
+"""
